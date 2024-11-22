05644a3952628740b31bcd96dfd3858f7e364ffa
szczuraszczyk <160737700+szczuraszczyk@users.noreply.github.com>
2024-11-22 13:22:52 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php
+    $db = new mysqli('localhost', 'root', '', 'biura_podrozy');
+    $sql = "SELECT dataWyjazdu, cel, cena FROM wycieczki WHERE dostepna = 1";
+    $result = $db->query($sql);
+    while($row = $result->fetch_assoc()) {
+      $date = $row['dataWyjazdu'];
+      $destination = $row['cel'] ;
+      $price = $row['cena'];
+
+      echo '<table><tr><th>dataWyjazdu</th><th>cel</th><th>cena</th></tr>';
+while($row = $result->fetch_assoc()) {
+  echo "<tr><td>{$row['dataWyjazdu']}</td><td>{$row['cel']}</td><td>{$row['cena']}</td></tr>";
+}
+echo '</table>';
+    }
+    $db->close();
+    ?>
+</body>
+</html>
